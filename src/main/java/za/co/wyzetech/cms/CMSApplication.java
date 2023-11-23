36018b6b554da68abb04d3757dac29caef4d64f8
Lanton H Vhengani <vhengani@gmail.com>
2023-11-23 08:18:10 +0200

    Add and configure Spring framework.
    Now, we are good to go.

--- src/main/java/za/co/wyzetech/cms/CMSApplication.java
+++ src/main/java/za/co/wyzetech/cms/CMSApplication.java
@@ -1,4 +1,33 @@
 package za.co.wyzetech.cms;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@Slf4j
+@SpringBootApplication
 public class CMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CMSApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            log.info("What do we have here:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                log.info(beanName);
+            }
+        };
+    }
 }
