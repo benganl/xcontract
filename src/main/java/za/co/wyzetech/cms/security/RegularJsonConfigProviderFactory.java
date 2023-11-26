08719dbeb1dfac60a928f22c9634807aaaa2a9a8
Lanton H Vhengani <vhengani@gmail.com>
2023-11-26 19:07:50 +0200

    Added embedded Keycload

---
+++ src/main/java/za/co/wyzetech/cms/security/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package za.co.wyzetech.cms.security;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
