26344dc9b3143febc7c6f93654660d2d1f73a1e4
Lanton H Vhengani <vhengani@gmail.com>
2023-12-02 15:17:32 +0200

    Min statemachine implementation.
    Now we move on to some Chuck Norris stuff.

---
+++ src/main/java/za/co/wyzetech/cms/statemachine/dto/StateMachineResponse.java
@@ -0,0 +1,18 @@
+package za.co.wyzetech.cms.statemachine.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class StateMachineResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private UUID stateItemId;
+    private String externalRef;
+    private String currentState;
+    private Boolean success;
+    private String message;
+}
--- src/main/java/za/co/wyzetech/cms/statemachine/stateitem/DefaultStateItemService.java
+++ src/main/java/za/co/wyzetech/cms/statemachine/stateitem/DefaultStateItemService.java
@@ -2,12 +2,29 @@ package za.co.wyzetech.cms.statemachine.stateitem;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+@Service
 class DefaultStateItemService implements StateItemService {
 
-    private StateItemRepository stateItemRepository;
+    private final StateItemRepository stateItemRepository;
+    
+    public DefaultStateItemService(StateItemRepository stateItemRepository) {
+	this.stateItemRepository = stateItemRepository;
+    }
 
     @Override
     public StateItem stateItemById(UUID id) {
 	return stateItemRepository.getById(id);
+    }
+
+    @Override
+    public StateItem create(StateItem stateItem) {
+	return stateItemRepository.save(stateItem);
+    }
+
+    @Override
+    public StateItem stateItemByExternalRef(String externalRef) {
+	return stateItemRepository.findByExternalRef(externalRef);
     }
 }
